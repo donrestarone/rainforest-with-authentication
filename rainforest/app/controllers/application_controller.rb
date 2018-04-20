8add639d159c6c1289eb327a774a358abe22d671
donrestarone <shashikejayatunge@gmail.com>
2018-04-20 12:41:30 -0400

    implement private method current user in application controller

--- rainforest/app/controllers/application_controller.rb
+++ rainforest/app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+private
+
+  	def current_user 
+		session[:user_id]
+	end 
+	
 end
