ed951b208fdb347574f61824e1f1e3af18ee214c
donrestarone <shashikejayatunge@gmail.com>
2018-04-20 12:16:30 -0400

    login working

--- rainforest/app/controllers/sessions_controller.rb
+++ rainforest/app/controllers/sessions_controller.rb
@@ -1,10 +1,19 @@
 class SessionsController < ApplicationController
 
   def new
-  	
+
   end
 
   def create
+  	u = User.find_by(email: params[:session][:email])
+
+  	if u && u.authenticate(params[:session][:password])
+  		#flash[:notice] = 'login success'
+  		redirect_to root_url
+  	else
+  		#flash[:error] = 'email or password invalid'
+  		render :new
+  	end 
   end
 
   def destroy
